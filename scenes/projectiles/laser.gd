e7584a2bd2e36d2ab18b27ae2d05b95d613eae35
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-08-18 19:16:13 -0500

    connected laser to the level

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,6 +3,18 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	$LaserTimer.start()
+
 func _process(delta):
 	position += direction * speed * delta
+
+# Laser collision and enemy hit
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	
+	queue_free()
+
+func _on_laser_timer_timeout():
+	queue_free()
