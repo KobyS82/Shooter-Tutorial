adefffd730c014150d0f6cf3bb2e617668f7188f
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-08-30 20:04:00 -0500

    added item spawns from objects

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -4,6 +4,9 @@ var rotation_speed: int = 3
 var type_options = ['laser', 'laser', 'laser', 'laser', 'grenade', 'health']
 var type = type_options[randi()%len(type_options)]
 
+var direction: Vector2
+var distance: int = randi_range(150, 250)
+
 func _ready():
 	if type == 'laser':
 		$Sprite2D.modulate = Color(0.1, 0.1, 0.4)
@@ -11,6 +14,13 @@ func _ready():
 		$Sprite2D.modulate = Color(0.8, 0.1, 0.1)
 	if type == 'health':
 		$Sprite2D.modulate = Color(0.1, 0.8, 0.1)
+	
+	#Tweens
+	var target_pos = position + direction * distance
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", target_pos, 0.5)
+	tween.tween_property(self, "scale", Vector2(1,1), 0.3,).from(Vector2(0,0))
 
 
 func _process(delta):
