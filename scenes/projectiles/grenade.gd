50a38ae498210370d50f18ab8d5c0a48da92e40f
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-13 20:24:00 -0500

    grenades now explode containers open

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -1,6 +1,17 @@
 extends RigidBody2D
 
 const speed = 750
+var exploded: bool = false
+var explosion_radius: int = 400
 
 func explode():
+	exploded = true
 	$AnimationPlayer.play("Explosion")
+
+func _process(_delta):
+	if exploded:
+		var targets = get_tree().get_nodes_in_group("Container")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < explosion_radius
+			if 'hit' in target and in_range:
+				target.hit()
