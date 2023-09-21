db8b7016efbad6ab62bac27291e09551da48127d
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-21 17:59:13 -0500

    drone enemy movement and health

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,10 +1,36 @@
 extends CharacterBody2D
 
+var active: bool = false
+var speed: int = 300
+
+var vulnerable: bool = true
+var health: int = 50
+
+func _ready():
+	$Explosion.hide()
+	#Don't need this
+#	$Sprite2D.show()
 
 func _physics_process(_delta):
-	var direction = Vector2.RIGHT
-	velocity = direction * 200
-	move_and_slide()
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		move_and_slide()
 
 func hit():
-	print("damaged")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$Sprite2D.material.set_shader_parameter("progress", 1)
+		$DamageTimer.start()
+	if health <= 0:
+		$AnimationPlayer.play("explosion")
+
+
+func _on_notice_area_body_entered(_body):
+	active = true
+
+
+func _on_damage_timer_timeout():
+	vulnerable = true
