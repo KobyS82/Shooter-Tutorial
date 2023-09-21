3eb1b8484926a8752cf4d9658c6de5a92df5a100
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-20 19:18:40 -0500

    bug enemie's code flows better, but animation gets janky

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-var player_nearby: bool = false
-var player_close: bool = false
+var active: bool = false
+var player_near: bool = false
 var can_attack: bool = true
 var damageable: bool = true
 
@@ -20,35 +20,36 @@ func hit():
 
 
 func _process(_delta):
-	if player_nearby and can_attack:
+	var direction: Vector2 = (Globals.player_pos - position).normalized()
+	velocity = direction * speed
+	if active:
 		look_at(Globals.player_pos)
-		var direction: Vector2 = (Globals.player_pos - position).normalized()
-		velocity = direction * speed
 		move_and_slide()
-		if player_close:
-			can_attack = false
-			velocity = Vector2.ZERO
-			$AnimatedSprite2D.play("attack")
-			$Timers/AttackTimer.start()
 
 
-func _on_notice_area_2d_body_entered(body):
-	player_nearby = true
+func _on_notice_area_2d_body_entered(_body):
+	active = true
 	$AnimatedSprite2D.play("walk")
 
-func _on_notice_area_2d_body_exited(body):
-	player_nearby = false
+func _on_notice_area_2d_body_exited(_body):
+	active = false
 	$AnimatedSprite2D.stop()
 
-func _on_attack_area_2d_body_entered(body):
-	player_close = true
+func _on_attack_area_2d_body_entered(_body):
+	player_near = true
+	$AnimatedSprite2D.play("attack")
 
-func _on_attack_area_2d_body_exited(body):
-	player_close = false
+func _on_attack_area_2d_body_exited(_body):
+	player_near = false
 
 func _on_damage_timer_timeout():
 	damageable = true
 	$AnimatedSprite2D.material.set_shader_parameter("progress", 0)
 
 func _on_attack_timer_timeout():
-	can_attack = true
+	$AnimatedSprite2D.play("attack")
+
+func _on_animated_sprite_2d_animation_finished():
+	if player_near:
+		Globals.health -= 10
+		$Timers/AttackTimer.start()
