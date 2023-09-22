90a342a0bd68a8f7e8676f6ba6b718bc4b52a7b0
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-21 20:07:02 -0500

    rework drone speed, added explosion damage

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 var active: bool = false
-var speed: float = 200.0
+var max_speed: int = 600
+var speed: int = 0
+var speed_Multiplier: int = 1
 
 var vulnerable: bool = true
 var health: int = 50
-var player_close: bool = false
+
+var explosion_active: bool = false
+var explosion_radius: int = 400
 
 func _ready():
 	$Explosion.hide()
@@ -15,12 +19,18 @@ func _physics_process(delta):
 	if active:
 		look_at(Globals.player_pos)
 		var direction = (Globals.player_pos - position).normalized()
-		velocity = direction * speed
-		speed += 1.0
+		velocity = direction * speed * speed_Multiplier
 		#stops on collision
 		var collision = move_and_collide(velocity * delta)
 		if collision:
 			$AnimationPlayer.play("explosion")
+			explosion_active = true
+	if explosion_active:
+		var targets = get_tree().get_nodes_in_group("Container") + get_tree().get_nodes_in_group("Entity")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < explosion_radius
+			if 'hit' in target and in_range:
+				target.hit()
 
 func hit():
 	if vulnerable:
@@ -30,23 +40,18 @@ func hit():
 		$Node/DamageTimer.start()
 	if health <= 0:
 		$AnimationPlayer.play("explosion")
+		explosion_active = true
 
+func stop_movement():
+	speed_Multiplier = 0
 
 func _on_notice_area_body_entered(_body):
 	active = true
+	var tween = create_tween()
+	tween.tween_property(self, "speed", max_speed, 6.0)
 
 
 func _on_damage_timer_timeout():
 	vulnerable = true
 	$Sprite2D.material.set_shader_parameter("progress", 0)
 
-
-func _on_explosion_area_2d_body_entered(_body):
-	player_close = true
-
-func _on_explosion_area_2d_body_exited(_body):
-	player_close = false
-
-func _on_animation_player_animation_started(explosion):
-	if player_close:
-		Globals.health -= 10
