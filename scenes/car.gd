f42e80d97021d4cca81fadab0c9f758c4db5d38a
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-26 20:18:08 -0500

    turret has lasers and fires, damages enemy

--- scenes/car.gd
+++ scenes/car.gd
@@ -2,15 +2,26 @@ extends PathFollow2D
 
 var player_near: bool = false
 
+@onready var line1: Line2D = $Turret/RayCast2D/Line2D
+@onready var line2: Line2D = $Turret/RayCast2D2/Line2D
+
+func fire():
+	Globals.health -= 20
+
+func _ready():
+	#Creates 2nd point of laser
+	line2.add_point($Turret/RayCast2D2.target_position)
+
 func _process(delta):
 	progress_ratio += 0.02 * delta
 	if player_near:
 		$Turret.look_at(Globals.player_pos)
+	$Turret/RayCast2D.get_collider()
 
 
 func _on_notice_area_body_entered(body):
 	player_near = true
-
+	$AnimationPlayer.play("laser_load")
 
 func _on_notice_area_body_exited(body):
 	player_near = false
