91ce7c440e0f6ef8b6294400758e917159485962
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-26 19:57:58 -0500

    added turret to car that follows the player

--- scenes/car.gd
+++ scenes/car.gd
@@ -1,4 +1,16 @@
 extends PathFollow2D
 
+var player_near: bool = false
+
 func _process(delta):
-	progress_ratio += 0.05 * delta
+	progress_ratio += 0.02 * delta
+	if player_near:
+		$Turret.look_at(Globals.player_pos)
+
+
+func _on_notice_area_body_entered(body):
+	player_near = true
+
+
+func _on_notice_area_body_exited(body):
+	player_near = false
