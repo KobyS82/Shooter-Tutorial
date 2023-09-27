b6f93def8979884ef301301c80624313d87c07f4
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-09-27 18:32:49 -0500

    lasers work fine and added fire animation

--- scenes/car.gd
+++ scenes/car.gd
@@ -11,17 +11,19 @@ func fire():
 func _ready():
 	#Creates 2nd point of laser
 	line2.add_point($Turret/RayCast2D2.target_position)
+	$FireSprites.hide()
 
 func _process(delta):
 	progress_ratio += 0.02 * delta
 	if player_near:
 		$Turret.look_at(Globals.player_pos)
+		$FireSprites.look_at(Globals.player_pos)
 	$Turret/RayCast2D.get_collider()
 
 
-func _on_notice_area_body_entered(body):
+func _on_notice_area_body_entered(_body):
 	player_near = true
 	$AnimationPlayer.play("laser_load")
 
-func _on_notice_area_body_exited(body):
+func _on_notice_area_body_exited(_body):
 	player_near = false
